067b569396fd4e68ecee24e4e6e4df9d6d3fd634
Anderson0312 <74934630+Anderson0312@users.noreply.github.com>
2022-04-20 03:14:15 -0300

    site com python templates

---
+++ meu_site.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# criar a 1° pagina do site
+# route -> JogosFavoritos.com/contas
+# função -> oque voce quer exibir naquela página
+
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
+
+@app.route("/Contas")
+def contas():
+    return render_template("contas.html")
+
+@app.route("/usuarios/<nome_usuario>")
+def usuarios(nome_usuario):
+    return nome_usuario
+
+#coloca o site no ar
+if __name__ == "__main__":
+    app.run(debug=True)
