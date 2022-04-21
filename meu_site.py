bac890fb25cfea7e94d148268068d1788474b10f
Anderson0312 <74934630+Anderson0312@users.noreply.github.com>
2022-04-21 00:47:53 -0300

    1

--- meu_site.py
+++ meu_site.py
@@ -9,14 +9,17 @@ app = Flask(__name__)
 def homepage():
     return render_template("homepage.html")
 
-@app.route("/Contas")
+@app.route("/contas")
 def contas():
     return render_template("contas.html")
 
 @app.route("/usuarios/<nome_usuario>")
 def usuarios(nome_usuario):
-    return nome_usuario
+    return render_template("usuarios.html", nome_usuario=nome_usuario)
 
 #coloca o site no ar
 if __name__ == "__main__":
     app.run(debug=True)
+    
+    
+    # servidor do heroku
